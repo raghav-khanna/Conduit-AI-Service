079b39a58bf3d3f866c8b3c038ecde7499bcfaa5
Pranav Chatur <pranavchatur@gmail.com>
2024-12-22 18:37:10 +0530

    Add AudioFileSplitter class, add LogHandling.py, initialise folder structure (#1)
    * add AudioFileSplitter class, add LogHandling.py, initialise folder structure
    
    * Add data folder
    
    * Add os.path.join instead of hardcoding path

---
+++ utils/LogHandling.py
@@ -0,0 +1,26 @@
+def log_err(message: str, disable: bool = False):
+    if disable:
+        log_disabled()
+        return
+    print('******\nERROR DETECTED: ' + str(message) + '\n******')
+
+
+def log_prog(message: str, disable: bool = False):
+    if disable:
+        log_disabled()
+        return
+    print('CHECKPOINT:' + str(message))
+
+
+def log_val(*args, value_name: str = '', disable: bool = False):
+    if disable:
+        log_disabled()
+        return
+    print('VALUE:', value_name, end = '')
+    print(*args)
+
+
+def log_disabled(disable: bool = False):
+    if disable:
+        return
+    print('DISABLED')
